c0e90fe9dfacdb2dc4251797d742071e91858f4c
Amábile <amabile.analistadesistemas@gmail.com>
2023-09-20 20:24:21 -0300

    inclusão da url admin e api nas urls

--- school_project/school_app/urls.py
+++ school_project/school_app/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from .views import StudentListCreatView, StudentDetailView, DisciplineListCreateView, DisciplineDatailView, TaskCreatView, TaskDetailView
+from django.contrib import admin
+from django.urls import path, include
 
 urlpatternes = [
   path('student/', StudentListCreatView.as_view(),name='student-list-create'),
@@ -8,4 +10,6 @@ urlpatternes = [
   path('discipline/<int:pk>/', DisciplineDatailView.as_view(), name='discipline-detail'),
   path('task/', TaskCreatView.as_view(), name='task-list-create'),
   path('task/<int:pk>/', TaskDetailView.as_view(), name='task-detail'),
+  path('admin/', admin.site.urls),
+  path('api/', include('school_app.urls')),
 ]
