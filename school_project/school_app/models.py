0b81300c58f8d333b6d102e86c29067743f74d2f
Amábile <amabile.analistadesistemas@gmail.com>
2023-09-20 20:39:22 -0300

    criação dos models

--- school_project/school_app/models.py
+++ school_project/school_app/models.py
@@ -1,1 +1,17 @@
 from django.db import models
+
+class Student(models.Model):
+  name = models.CharField(max_length=100)
+  email = models.EmailField(unique=True)
+
+class Discpline(models.Model):
+  name = models.CharField(max_length=100)
+  description = models.TextField()
+
+class Task(models.Model):
+  title = models.CharField(max_length=100) 
+  description = models.TextField()
+  delivery_date = models.DateField()
+  completed = models.BooleanField(default=False) 
+  student = models.ForeignKey(Student, on_delete=models.CASCADE)
+  discipline = models.ManyToManyField(Discpline)
