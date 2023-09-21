3a077306f93d8a85eb6205b9bff5f9b4ff5f2c20
Amábile <amabile.analistadesistemas@gmail.com>
2023-09-21 13:45:37 -0300

    refiz o views utilizando os metodos

--- school_project/school_app/views.py
+++ school_project/school_app/views.py
@@ -1,29 +1,82 @@
-from django.shortcuts import render
-from rest_framework import generics
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
 from .models import Student, Discipline, Task
 from .serializers import StudentSerializer, DisciplineSerializer, TaskSerializer
-# Create your views here.
 
-class StudentListCreatView(generics.ListCreateAPIView):
-  queryset = Student.objects.all()
-  serializer_class = StudentSerializer
+class StudentListCreateView(APIView):
+    def get(self, request):
+        students = Student.objects.all()
+        serializer = StudentSerializer(students, many=True)
+        return Response(serializer.data)
 
-class StudentDetailView(generics.RetrieveUpdateAPIView):
-  queryset = Student.objects.all()
-  serializer_class = StudentSerializer
+    def post(self, request):
+        serializer = StudentSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-class DisciplineListCreateView(generics.ListCreateAPIView):
-  queryset = Discipline.objects.all()
-  serializer_class = DisciplineSerializer
+class StudentDetailView(APIView):
+    def get(self, request, pk):
+        student = Student.objects.get(pk=pk)
+        serializer = StudentSerializer(student)
+        return Response(serializer.data)
 
-class DisciplineDatailView(generics.RetrieveUpdateDestroyAPIView):
-  queryset = Discipline.objects.all()
-  serializer_class = DisciplineSerializer
+    def put(self, request, pk):
+        student = Student.objects.get(pk=pk)
+        serializer = StudentSerializer(student, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-class TaskCreatView(generics.ListCreateAPIView):
-  queryset = Task.objects.all()
-  serializer_class = TaskSerializer
+    def delete(self, request, pk):
+        student = Student.objects.get(pk=pk)
+        student.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
 
-class TaskDetailView(generics.RetrieveUpdateDestroyAPIView):
-  queryset = Task.objects.all()
-  serializer_class = TaskSerializer
+class DisciplineListCreateView(APIView):
+    def get(self, request):
+        disciplines = Discipline.objects.all()
+        serializer = DisciplineSerializer(disciplines, many=True)
+        return Response(serializer.data)
+
+    def post(self, request):
+        serializer = DisciplineSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class DisciplineDetailView(APIView):
+    def get(self, request, pk):
+        discipline = Discipline.objects.get(pk=pk)
+        serializer = DisciplineSerializer(discipline)
+        return Response(serializer.data)
+
+    def put(self, request, pk):
+        discipline = Discipline.objects.get(pk=pk)
+        serializer = DisciplineSerializer(discipline, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk):
+        discipline = Discipline.objects.get(pk=pk)
+        discipline.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+class TaskCreateView(APIView):
+    def get(self, request):
+        tasks = Task.objects.all()
+        serializer = TaskSerializer(tasks, many=True)
+        return Response(serializer.data)
+
+    def post(self, request):
+        serializer = TaskSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
