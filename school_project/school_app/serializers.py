01b5cb900289d8da036f881e8c0b57e97004aab5
Amábile <amabile.analistadesistemas@gmail.com>
2023-09-20 21:52:19 -0300

    comentando o serializer.py

--- school_project/school_app/serializers.py
+++ school_project/school_app/serializers.py
@@ -1,20 +1,31 @@
+# Importa o módulo 'serializers' do Django REST framework, que é usado para criar serializadores.
 from rest_framework import serializers
+# Importa os modelos (Student, Discipline, Task) do seu aplicativo para serem usados nos serializadores.
 from .models import Student, Discipline, Task
-# Create your models here.
 
+# Define o serializador 'StudentSerializer' para o modelo 'Student'.
 class StudentSerializer(serializers.ModelSerializer):
   class Meta:
+    # Especifica o modelo a ser serializado, que é 'Student'.
     model = Student
+    # Especifica que todos os campos do modelo 'Student' devem ser incluídos na serialização.
     fields = '__all__'
 
+# Define o serializador 'DisciplineSerializer' para o modelo 'Discipline'.
 class DisciplineSerializer(serializers.ModelSerializer):
-  class Mata:
+  class Meta:
+    # Especifica o modelo a ser serializado, que é 'Discipline'.
     model = Discipline
+    # Especifica que todos os campos do modelo 'Discipline' devem ser incluídos na serialização.
     fields = '__all__'
 
+# Define o serializador 'TaskSerializer' para o modelo 'Task'.
 class TaskSerializer(serializers.ModelSerializer):
   class Meta:
+    # Especifica o modelo a ser serializado, que é 'Task'.
     model = Task
-    fields = '__all__'    
+    # Especifica que todos os campos do modelo 'Task' devem ser incluídos na serialização.
+    fields = '__all__'
+
 
     
